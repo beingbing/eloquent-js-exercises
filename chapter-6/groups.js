4f40fd9d9671bc60b9dd3f1bde2034d9d0057fea
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-24 04:13:01 +0530

    sat 24 jul 21 | 02 | groups

---
+++ chapter-6/groups.js
@@ -0,0 +1,33 @@
+class Group {
+  constructor() {
+    this.arr = new Array();
+  }
+
+  add(val) {
+    if (!this.has(val)) this.arr.push(val);
+  }
+
+  delete(val) {
+    this.arr = this.arr.filter((v) => v !== val);
+  }
+
+  has(val) {
+    return this.arr.includes(val);
+  }
+
+  static from = (a) => {
+    let grp = new Group();
+    for (let i of a) grp.add(i);
+    return grp;
+  };
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
